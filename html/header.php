17632139fcc4da32feccb2b4e23d31c9167edf74
Amanar Marouane <marouane.amanar07@gmail.com>
2024-12-31 09:36:19 +0100

    pushing to dev branch

--- html/header.php
+++ html/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Task</title>
+    <title>GameVault</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="./../js/script.js"></script>
     <link rel="stylesheet" href="./../CSS/style.css">
--- html/profile_edit.php
+++ html/profile_edit.php
@@ -7,7 +7,7 @@ include_once("./../html/header.php");
         <span class="block sm:inline"><?= htmlspecialchars($_GET['error']); ?></span>
         <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" onclick="this.parentElement.style.display='none';">
             <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <title>Edit</title>
+                <title></title>
                 <path d="M14.348 5.652a1 1 0 011.415 0l.086.086a1 1 0 010 1.415L11.415 11l4.434 4.434a1 1 0 01-1.415 1.415L10 12.415l-4.434 4.434a1 1 0 01-1.415-1.415L8.585 11 4.152 6.566a1 1 0 011.415-1.415L10 9.585l4.348-4.348z" />
             </svg>
         </button>
